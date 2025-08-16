f8a2b28c225e06f1cc54abfc625d69a00ab7b68e
Marcel Marsico <marsico.1094@gmail.com>
2025-08-16 12:17:29 -0300

    comentario agregado

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,8 +1,9 @@
 ﻿Console.WriteLine(Addition.Add(7, 2));
 Console.WriteLine(Substraction.Substract(10, 1));
-Console.WriteLine(multiplicacion.Multiply(3, 3));
+Console.WriteLine(Multiplication.Multiply(3, 3));
 Console.WriteLine(Division.Divide(18, 2));
 
+// Esta clase implementa la operación <nombre-operación>
 public class Multiplication
 {
     public static int Multiply(int a, int b)
