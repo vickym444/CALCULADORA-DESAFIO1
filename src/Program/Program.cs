e7b00c7d3a26c73c5a2806059f2edeee4e51ef40
vickym444 <male.joaco@gmail.com>
2025-08-16 12:06:43 -0300

    Implementacion de la operacion DIVISION

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -3,12 +3,13 @@ Console.WriteLine(Substraction.Substract(10, 1));
 Console.WriteLine(Multiplication.Multiply(3, 3));
 Console.WriteLine(Division.Divide(18, 2));
 
-//Esta clase implementa la operacion SUMA
-public class Addition
+
+
+public class Division
 {
-    public static int Add(int a, int b)
+    public static double Divide(int a, int b)
     {
-        return a + b;
+        return (double)a / b;
     }
 }
 
