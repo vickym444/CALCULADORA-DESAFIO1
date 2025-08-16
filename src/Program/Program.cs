6e1d828d87bb3c754acb9c2754abe1a9cecc44d2
CarolinaSilva1502 <carolina.silva@estudiantes.utec.edu.uy>
2025-08-16 11:55:07 -0300

    Implementación de la operación RESTA

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -2,3 +2,11 @@
 Console.WriteLine(Substraction.Substract(10, 1));
 Console.WriteLine(Multiplication.Multiply(3, 3));
 Console.WriteLine(Division.Divide(18, 2));
+
+public class Substraction
+{
+   public static int Substract(int a, int b)
+   {
+       return a - b;
+   }
+}
