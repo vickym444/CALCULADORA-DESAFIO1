dd432d116ab47b74dab88e5bcb095b47cb13e767
CarolinaSilva1502 <carolina.silva@estudiantes.utec.edu.uy>
2025-08-16 13:33:55 -0300

    finalizado

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -3,11 +3,34 @@ Console.WriteLine(Substraction.Substract(10, 1));
 Console.WriteLine(Multiplication.Multiply(3, 3));
 Console.WriteLine(Division.Divide(18, 2));
 
-//Esta clase implementa la operacion SUMA
 public class Addition
 {
     public static int Add(int a, int b)
     {
         return a + b;
+    }
+}
+
+public class Substraction
+{
+    public static int Substract(int a, int b)
+    {
+        return a - b;
+    }
+}
+
+public class Multiplication
+{
+    public static int Multiply(int a, int b)
+    {
+        return a * b;
+    }
+}
+
+public class Division
+{
+    public static double Divide(int a, int b)
+    {
+        return (double)a / b;
     }
 }
