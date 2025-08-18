ac544191efdd959988a1c63b90ac669037c632ef
CarolinaSilva1502 <carolina.silva@estudiantes.utec.edu.uy>
2025-08-18 18:52:16 -0300

    cambio incorrecto

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,7 +1,7 @@
-﻿Console.WriteLine(Addition.Add(7, 2));
+﻿//Console.WriteLine(Addition.Add(7, 2));
 Console.WriteLine(Substraction.Substract(10, 1));
-Console.WriteLine(Multiplication.Multiply(3, 3));
-Console.WriteLine(Division.Divide(18, 2));
+//Console.WriteLine(Multiplication.Multiply(3, 3));
+//Console.WriteLine(Division.Divide(18, 2));
 
 // Esta clase implementa la operación RESTA
 
